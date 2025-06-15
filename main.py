232ab89aef3e539bd0272b48d8ce9818ad4db162
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2025-06-15 15:02:38 +0200

    Define basic main structure with initialization of water shallow models

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from typer import typer
+from typer import Typer
+from random_manager import RandomGenerators
+from msw_model import ModifiedShallowWaterModel
+from settings import load_settings
 
-app = typer.Typer()
+app = Typer()
+settings = load_settings()
+rngs = RandomGenerators.from_seed(base_seed=42)
 
 
-@app.command
+@app.command()
 def main():
     pass
 
@@ -11,11 +16,21 @@ def main():
 methods = ["EnKF", "NN", "QPEns"]
 
 
-@app.commnd
+@app.command()
+def initialize_msw_model(ensemble_members: int = 10):
+    state_truth = ModifiedShallowWaterModel(
+        num_ensemble_members=1, random_generator=rngs.truth_rng
+    )
+    state_ensemble = ModifiedShallowWaterModel(
+        num_ensemble_members=ensemble_members, random_generator=rngs.ensemble_rng
+    )
+
+
+@app.command()
 def train_nn():
     pass
 
 
-@app.commnd
+@app.command()
 def assimilate():
     pass
