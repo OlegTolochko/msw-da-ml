5d8b578fbcc9b266880c0d387c89b4e369e95417
oleg <oleg.i.tolochko@gmail.com>
2025-07-08 14:38:31 +0200

    Add permutation to train val loaders and adjust training_config name

--- train_nn.py
+++ train_nn.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader, TensorDataset
 app = Typer()
 
 settings = load_settings()
-nn_config = settings.nn_config
+training_config = settings.training_config
 
 
 @app.command()
@@ -19,6 +19,7 @@ def load_generated_data(pipeline_state_name: str):
     data = DataGenerationPipeline.load_pipeline_state(
         pipeline_state_name=pipeline_state_name
     )
+    print(data.histories["kf"][0].shape)
     return data
 
 
@@ -34,25 +35,25 @@ def get_train_val_loaders(pipeline_state_name: str):
     kf_train, kf_val, qp_train, qp_val = train_test_split(
         kf_data,
         qp_data,
-        test_size=nn_config.val_split_size,
-        random_state=nn_config.random_state_train_test_split,
+        test_size=training_config.val_split_size,
+        random_state=training_config.random_state_train_test_split,
     )
 
     train_dataset = TensorDataset(
-        torch.tensor(kf_train, dtype=torch.float32),
-        torch.tensor(qp_train, dtype=torch.float32),
+        torch.tensor(kf_train, dtype=torch.float32).permute(2, 0, 1),
+        torch.tensor(qp_train, dtype=torch.float32).permute(2, 0, 1),
     )
 
     val_dataset = TensorDataset(
-        torch.tensor(kf_val, dtype=torch.float32),
-        torch.tensor(qp_val, dtype=torch.float32),
+        torch.tensor(kf_val, dtype=torch.float32).permute(2, 0, 1),
+        torch.tensor(qp_val, dtype=torch.float32).permute(2, 0, 1),
     )
 
     train_loader = DataLoader(
-        train_dataset, batch_size=nn_config.batch_size, shuffle=True
+        train_dataset, batch_size=training_config.batch_size, shuffle=True
     )
 
-    val_loader = DataLoader(val_dataset, batch_size=nn_config.batch_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=training_config.batch_size, shuffle=False)
 
     return train_loader, val_loader
 
