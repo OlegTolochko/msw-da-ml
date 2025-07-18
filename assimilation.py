d524eb1a4307526468b64ff627b3028d4bb5a539
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2025-07-18 17:49:26 +0200

    Begin restructuring assimilation

--- assimilation.py
+++ assimilation.py
@@ -1,9 +1,105 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 from settings import load_settings
 import cvxopt
 
 settings = load_settings()
 obs_config = settings.observation_generation_config
+
+
+class BaseAssimilation(ABC):
+    def calculate_gaspari_cohn(self, z: np.ndarray):
+        """
+        Calculates the Gaspari-Cohn function for normalized distances z.
+        Effectively ensures that neighbors that are further away are weighted less.
+        """
+        rho = np.zeros_like(z, dtype=float)
+
+        mask1 = (z >= 0) & (z < 1)
+        rho[mask1] = (
+            -0.25 * z[mask1] ** 5
+            + 0.5 * z[mask1] ** 4
+            + (5.0 / 8.0) * z[mask1] ** 3
+            - (5.0 / 3.0) * z[mask1] ** 2
+            + 1.0
+        )
+
+        mask2 = (z >= 1) & (z < 2)
+        rho[mask2] = (
+            (1.0 / 12.0) * z[mask2] ** 5
+            - 0.5 * z[mask2] ** 4
+            + (5.0 / 8.0) * z[mask2] ** 3
+            + (5.0 / 3.0) * z[mask2] ** 2
+            - 5.0 * z[mask2]
+            + 4.0
+            - (2.0 / 3.0) * (1.0 / z[mask2])
+        )
+
+        return rho
+
+    def calculate_localization_matrix(self, num_grid_points: int, grid_point_influence: int):
+        """
+        Calculates the localization matrix.
+        Limits neighboring grid cell influence with a max influence distance of grid_point_influence.
+        """
+        if grid_point_influence == 0:
+            return np.identity(num_grid_points * 3)
+
+        indices = np.arange(num_grid_points)
+        distances = np.abs(indices - indices[:, np.newaxis])
+        periodic_distances = np.minimum(distances, num_grid_points - distances)
+
+        z = periodic_distances / float(grid_point_influence)
+
+        localization_matrix = self.calculate_gaspari_cohn(z)
+
+        expanded_localization_matrix = np.tile(localization_matrix, (3, 3))
+
+        return expanded_localization_matrix
+    
+    def calculate_obs_covariance_error(self, num_grid_points, observation_position_flat):
+        u_var = np.tile(obs_config.u_error_std**2, num_grid_points)
+        h_var = np.tile(obs_config.h_error_std**2, num_grid_points)
+        r_var = np.tile(
+            np.exp(obs_config.r_error_std**2)
+            * np.exp(2 * obs_config.r_error_mean + obs_config.r_error_std**2),
+            num_grid_points,
+        )
+        var_flat = np.concat([u_var, h_var, r_var])[observation_position_flat]
+        return var_flat
+
+
+    def calculate_background_covariance_error(self,
+        ensemble: np.ndarray, num_ensemble_members: int, num_grid_points: int
+    ):
+        """
+        Calculates the background error covariance matrix from ensemble deviations.
+
+        It calculates the uncertainty (variance) at each individual grid points across ensemble members
+        and the correlations between different grid points and the variables (u, h, r), with a max
+        max influence range of grid_point_influence.
+        """
+        ens_mean_difference = np.sqrt(
+            obs_config.cov_inflation / (num_ensemble_members - 1)
+        ) * (ensemble - np.mean(ensemble, axis=2, keepdims=True))
+        flat_state = np.concat(
+            ens_mean_difference, axis=0
+        )  # flattend to compute cov. err for grid point in all domains (u, h, r) to each other grid point
+        cov_error = np.dot(flat_state, flat_state.T)
+        cov_error = cov_error * self.calculate_localization_matrix(
+            num_grid_points=num_grid_points,
+            grid_point_influence=obs_config.grid_point_influence,
+        )
+        return cov_error
+
+
+class QPEnsembleAssimilation(BaseAssimilation):
+    pass
+
+
+class EnKFAssimilation(BaseAssimilation):
+    pass
 
 
 def generate_observation(
@@ -143,56 +239,6 @@ def qpens_assimilate(ensemble, observation, observation_position):
     return ensemble_updated
 
 
-def calculate_gaspari_cohn(z: np.ndarray):
-    """
-    Calculates the Gaspari-Cohn function for normalized distances z.
-    Effectively ensures that neighbors that are further away are weighted less.
-    """
-    rho = np.zeros_like(z, dtype=float)
-
-    mask1 = (z >= 0) & (z < 1)
-    rho[mask1] = (
-        -0.25 * z[mask1] ** 5
-        + 0.5 * z[mask1] ** 4
-        + (5.0 / 8.0) * z[mask1] ** 3
-        - (5.0 / 3.0) * z[mask1] ** 2
-        + 1.0
-    )
-
-    mask2 = (z >= 1) & (z < 2)
-    rho[mask2] = (
-        (1.0 / 12.0) * z[mask2] ** 5
-        - 0.5 * z[mask2] ** 4
-        + (5.0 / 8.0) * z[mask2] ** 3
-        + (5.0 / 3.0) * z[mask2] ** 2
-        - 5.0 * z[mask2]
-        + 4.0
-        - (2.0 / 3.0) * (1.0 / z[mask2])
-    )
-
-    return rho
-
-
-def calculate_localization_matrix(num_grid_points: int, grid_point_influence: int):
-    """
-    Calculates the localization matrix.
-    Limits neighboring grid cell influence with a max influence distance of grid_point_influence.
-    """
-    if grid_point_influence == 0:
-        return np.identity(num_grid_points * 3)
-
-    indices = np.arange(num_grid_points)
-    distances = np.abs(indices - indices[:, np.newaxis])
-    periodic_distances = np.minimum(distances, num_grid_points - distances)
-
-    z = periodic_distances / float(grid_point_influence)
-
-    localization_matrix = calculate_gaspari_cohn(z)
-
-    expanded_localization_matrix = np.tile(localization_matrix, (3, 3))
-
-    return expanded_localization_matrix
-
 
 def kf_assimilate(ensemble, observation, observation_position):
     """
@@ -242,39 +288,3 @@ def kf_assimilate(ensemble, observation, observation_position):
     ensemble_updated = np.stack([u_update, h_update, r_update])
 
     return ensemble_updated
-
-
-def calculate_obs_covariance_error(num_grid_points, observation_position_flat):
-    u_var = np.tile(obs_config.u_error_std**2, num_grid_points)
-    h_var = np.tile(obs_config.h_error_std**2, num_grid_points)
-    r_var = np.tile(
-        np.exp(obs_config.r_error_std**2)
-        * np.exp(2 * obs_config.r_error_mean + obs_config.r_error_std**2),
-        num_grid_points,
-    )
-    var_flat = np.concat([u_var, h_var, r_var])[observation_position_flat]
-    return var_flat
-
-
-def calculate_background_covariance_error(
-    ensemble: np.ndarray, num_ensemble_members: int, num_grid_points: int
-):
-    """
-    Calculates the background error covariance matrix from ensemble deviations.
-
-    It calculates the uncertainty (variance) at each individual grid points across ensemble members
-    and the correlations between different grid points and the variables (u, h, r), with a max
-    max influence range of grid_point_influence.
-    """
-    ens_mean_difference = np.sqrt(
-        obs_config.cov_inflation / (num_ensemble_members - 1)
-    ) * (ensemble - np.mean(ensemble, axis=2, keepdims=True))
-    flat_state = np.concat(
-        ens_mean_difference, axis=0
-    )  # flattend to compute cov. err for grid point in all domains (u, h, r) to each other grid point
-    cov_error = np.dot(flat_state, flat_state.T)
-    cov_error = cov_error * calculate_localization_matrix(
-        num_grid_points=num_grid_points,
-        grid_point_influence=obs_config.grid_point_influence,
-    )
-    return cov_error
