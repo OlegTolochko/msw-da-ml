217fab3ed53ce1cb41231275f05c84e04a4fc3d8
oleg <oleg.i.tolochko@gmail.com>
2025-08-12 15:20:49 +0200

    Correct pinball loss implementation

--- training/losses.py
+++ training/losses.py
@@ -39,4 +39,4 @@ def pinball_loss(y_true: torch.Tensor, y_pred: torch.Tensor, tau: float):
     err = y_true - y_pred
 
     loss = torch.where(err > 0, tau * (err), (1 - tau) * (-err))
-    return loss
+    return torch.mean(loss)
