0d65e8bed7b043694f44431967348c3c29662d18
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2025-08-12 13:54:34 +0200

    Implement basic pinball loss for quantile regression

--- training/losses.py
+++ training/losses.py
@@ -33,3 +33,10 @@ class RMSEBiasLoss(nn.Module):
         loss = rmse_loss + loss_config.bias_loss_weight * bias_loss
 
         return loss
+
+
+def pinball_loss(y_true: torch.Tensor, y_pred: torch.Tensor, tau: float):
+    err = y_true - y_pred
+
+    loss = torch.where(err > 0, tau * (err), (1 - tau) * (-err))
+    return loss
