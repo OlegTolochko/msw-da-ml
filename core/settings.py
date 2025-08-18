d0b860ae9f70bb254566b6a2946e2b96e68630c2
oleg <oleg.i.tolochko@gmail.com>
2025-08-18 12:59:52 +0200

    Add new required paths for quantile regression to config

--- core/settings.py
+++ core/settings.py
@@ -94,6 +94,7 @@ class GlobalConfig(BaseModel):
     normalization_out_filename: str
     experiment_histories_out_filename: str
     trained_quantile_nn_model_out_filename: str
+    quantile_normalization_out_filename: str
     base_seed: int
 
 
