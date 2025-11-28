8e3c377aae0ef10eaa5cfda79786c90175017b46
oleg <oleg.i.tolochko@gmail.com>
2025-11-28 15:18:59 +0100

    Add beta weighting factor to gaussian nll loss

--- msw_da_ml/evidential_regression/losses.py
+++ msw_da_ml/evidential_regression/losses.py
@@ -3,8 +3,9 @@ import torch
 import torch.nn as nn
 
 class GaussianNLL(nn.Module):
-    def __init__(self, logvar_min: float = -10.0, logvar_max: float = 5.0):
+    def __init__(self, beta: float = 0.5, logvar_min: float = -6.0, logvar_max: float = 2.0):
         super().__init__()
+        self.beta = beta
         self.log2pi = math.log(2 * math.pi)
         self.logvar_min = logvar_min
         self.logvar_max = logvar_max
@@ -14,6 +15,12 @@ class GaussianNLL(nn.Module):
         log_var = torch.clamp(log_var, self.logvar_min, self.logvar_max)
 
         err = y - mu
-        nll = 0.5 * (self.log2pi + log_var + torch.exp(-log_var) * err**2)
-        loss = nll.mean()
-        return loss
+        precision = torch.exp(-log_var)
+        
+        precision_detached = precision.detach()
+        
+        precision_weighted = precision_detached ** (1 - self.beta) * precision ** self.beta
+        
+        nll = 0.5 * (self.log2pi + log_var + precision_weighted * err**2)
+        
+        return nll.mean()
