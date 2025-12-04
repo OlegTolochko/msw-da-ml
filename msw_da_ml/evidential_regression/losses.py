4bbad407799fc1fb8911468f26d5898cd7258aed
oleg <oleg.i.tolochko@gmail.com>
2025-12-04 15:19:30 +0100

    Add new loss for enforcing normal inverse gamma distribution with associated model outputting such a distribution

--- msw_da_ml/evidential_regression/losses.py
+++ msw_da_ml/evidential_regression/losses.py
@@ -2,8 +2,11 @@ import math
 import torch
 import torch.nn as nn
 
+
 class GaussianNLL(nn.Module):
-    def __init__(self, beta: float = 0.5, logvar_min: float = -6.0, logvar_max: float = 2.0):
+    def __init__(
+        self, beta: float = 0.5, logvar_min: float = -6.0, logvar_max: float = 2.0
+    ):
         super().__init__()
         self.beta = beta
         self.log2pi = math.log(2 * math.pi)
@@ -11,16 +14,38 @@ class GaussianNLL(nn.Module):
         self.logvar_max = logvar_max
 
     def forward(self, y, mu, log_var):
-
         log_var = torch.clamp(log_var, self.logvar_min, self.logvar_max)
 
         err = y - mu
         precision = torch.exp(-log_var)
-        
+
         precision_detached = precision.detach()
-        
-        precision_weighted = precision_detached ** (1 - self.beta) * precision ** self.beta
-        
+
+        precision_weighted = (
+            precision_detached ** (1 - self.beta) * precision**self.beta
+        )
+
         nll = 0.5 * (self.log2pi + log_var + precision_weighted * err**2)
-        
+
         return nll.mean()
+
+
+class NIGLoss(nn.Module):
+    def __init__(self, reg_coef: float = 1.0):
+        super().__init__()
+        self.reg_coef = reg_coef
+
+    def forward(self, gamma, nu, alpha, beta, y):
+        omega = 2 * beta * (1 + nu)
+        L_nll = (
+            1 / 2 * torch.log(torch.pi / nu)
+            - alpha * torch.log(omega)
+            + (alpha + 1 / 2) * torch.log((y - gamma) ** 2 * nu + omega)
+            + torch.log(
+                torch.lgamma(alpha)
+                / torch.lgamma(alpha + 1 / 2)
+            )
+        )
+        L_r = torch.abs(y - gamma) * (2 * nu + alpha)
+        loss = torch.mean(L_nll + self.reg_coef * L_r)
+        return loss
---
+++ msw_da_ml/evidential_regression/nig_network.py
@@ -0,0 +1,60 @@
+import torch.nn.functional as F
+import torch.nn as nn
+
+from msw_da_ml.settings import load_settings
+
+settings = load_settings()
+network_config = settings.network_config
+
+
+class NIGCNNModel(nn.Module):
+    def __init__(self, dropout=0.1):
+        super().__init__()
+
+        layers = []
+        layers += [
+            nn.Conv1d(
+                in_channels=network_config.in_channels,
+                out_channels=network_config.hidden_channels,
+                kernel_size=network_config.kernel_size,
+                padding=network_config.kernel_size // 2,
+                padding_mode="circular",
+            ),
+            nn.SELU(),
+            nn.Dropout(p=dropout),
+        ]
+
+        for i in range(network_config.num_layers - 2):
+            layers += [
+                nn.Conv1d(
+                    in_channels=network_config.hidden_channels,
+                    out_channels=network_config.hidden_channels,
+                    kernel_size=network_config.kernel_size,
+                    padding=network_config.kernel_size // 2,
+                    padding_mode="circular",
+                ),
+                nn.SELU(),
+                nn.Dropout(p=dropout),
+            ]
+
+        layers += [
+            nn.Conv1d(
+                in_channels=network_config.hidden_channels,
+                out_channels=12,
+                kernel_size=network_config.kernel_size,
+                padding=network_config.kernel_size // 2,
+                padding_mode="circular",
+            ),
+        ]
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.network(x)
+        gamma, nu, alpha, beta = x[:, :3], x[:, 3:6], x[:, 6:9], x[:, 9:]
+        nu = F.softplus(nu)
+        beta = F.softplus(beta)
+        alpha = F.softplus(alpha) + 1
+
+        gamma[:, 2] = F.softplus(gamma[:, 2])
+        return gamma, nu, alpha, beta
